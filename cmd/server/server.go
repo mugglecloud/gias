967a99e2865cd18efd47102102da57e32f02d157
mugglecloud <62490785+mugglecloud@users.noreply.github.com>
2021-04-20 12:45:48 +0800

    first commit

---
+++ cmd/server/server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+const DEFAULT_ADDRESS = "0.0.0.0:8080"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(" Gorilla!\n"))
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Add your routes as needed
+	r.HandleFunc("/", handler)
+
+	srv := &http.Server{
+		Addr: DEFAULT_ADDRESS,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	log.Printf("server started at http://%s ...\n", DEFAULT_ADDRESS)
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	wait, _ := time.ParseDuration("15s")
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mugglecloud/gias
+
+go 1.16
+
+require (
+	github.com/cosmtrek/air v1.26.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/pelletier/go-toml v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20210419170143-37df388d1f33 // indirect
+)
